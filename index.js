9ada8f84e0c2821ee2fd9227a6552a47eedcfcc1
Jason Pollack <jasonpollack87@gmail.com>
2018-03-23 01:44:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function shout(string) {
   return string.toUpperCase()
 }
 
-console.log logShout(string) {
-  return string.toUpperCase()
+function whisper(string) {
+  return string.toLowerCase()
 }
